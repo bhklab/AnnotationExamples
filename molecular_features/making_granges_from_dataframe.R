072ab5563897c048b06f3d9fd9783c36227d91ff
Christopher Eeles <christopher.eeles@outlook.com>
2022-06-23 14:55:40 -0400

    Imprvode duplicate gene handling to catch cases of ambiguity and error

--- molecular_features/making_granges_from_dataframe.R
+++ molecular_features/making_granges_from_dataframe.R
@@ -20,7 +20,7 @@ download.file(
 
 # get features_gene table
 gencodev33 <- as.data.table(get(object_names[2]))
-# remove versions to match the
+# remove versions to match the identifiers from our example data
 gencodev33[, gene_id_no_ver := gsub("\\..*$", "", gene_id)]
 
 # -- Attach to our feature data
@@ -33,25 +33,34 @@ ranged_features <- merge.data.table(
     all.x=TRUE
 )
 
-# handle unmapped features
+# handle unmapped features by adding dummy genomic coordinates
 ranged_features[
     is.na(start),
     c("start", "end", "length", "strand") := list(-1, -1, 0, "*")
 ]
-# drop Y chromosome version if duplicated genes
-# (lexically X is before Y so first always select X)
-ranged_features <- ranged_features[, lapply(.SD, first), by=gene_id]
 
-# make sure duplicated were only due to chromosomes
+# drop Y chromosome version if duplicated genes
+## This statement does a select for rows into groups defined by rownames
+## The statement is only applied in cases where there are more than 1 items in
+##   a group. I.e., where there are duplicated rownames!
+## See: https://rdatatable.gitlab.io/data.table/articles/datatable-sd-usage.html
+ranged_features <- ranged_features[,
+    if (.N > 1) .SD[seqnames != "chrY", ] else .SD,
+    by=rownames
+]
+
+# make sure duplicated were only due to chrY
+# if this fails we have more complexity to handle!
 stopifnot(all(ranged_features$rownames == features$features))
 
-# build the GRanges object
+# -- build the GRanges object and RangedSummarizedExperiment
 row_ranges <- makeGRangesFromDataFrame(
     ranged_features,
     keep.extra.columns=TRUE  # retain metadata
 )
 names(row_ranges) <- row_ranges$rownames
 
+# for your case, you will probably need to curate the other slots yourself
 RSE <- SummarizedExperiment(
     assays=assays(SE),
     rowRanges=row_ranges,
